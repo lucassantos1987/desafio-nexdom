a2286ca10de459ec905d388108073b0ad5df7521
Lucas Santos <lucas.rafael.silva.santos@gmail.com>
2025-07-23 12:46:14 -0300

    Ajuste tela inicial front-end

--- web/src/components/DesafioNexDom.vue
+++ web/src/components/DesafioNexDom.vue
@@ -2,8 +2,7 @@
   <div class="flex flex-col justify-center p-4 mt-8">
     <h1 class="text-4xl font-bold">Desafio NEXDOM - FullStack (Java + VueJS)</h1>
     <br>
-    <p class="text-2xl">Construir uma aplicação RESTful para controle de estoque com no mínimo duas entidades, como por
-      exemplo: Produto e Movimento Estoque.
+    <p class="text-2xl">Aplicação RESTful para controle de estoque.
     </p>
   </div>
 </template>
